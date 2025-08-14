ebca98d84e9de5ff9ebaf9f181d759a666149065
Thomas Copin <thomas.copin@infrabel.be>
2025-08-14 14:25:18 +0200

    Adding 404 page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ListPokemonComponent } from './features/pokemon/list-pokemon/list-pokemon.component';
 import { DetailPokemonComponent } from './features/pokemon/detail-pokemon/detail-pokemon.component';
+import { PageNotFoundComponent } from './features/errors/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: 'pokemon', component: ListPokemonComponent },
     { path: 'pokemon/:id', component: DetailPokemonComponent },
-    { path: '', redirectTo: 'pokemon', pathMatch: 'full'}
+    { path: '', redirectTo: 'pokemon', pathMatch: 'full'},
+    { path: '**', component: PageNotFoundComponent}
 ];
