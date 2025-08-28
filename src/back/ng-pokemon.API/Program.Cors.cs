204fa9d6817746c0363d7d562ee9e9649794bd8b
Thomas Copin <thomas.copin@infrabel.be>
2025-08-28 16:19:49 +0200

    setup partial program.cs and adding cors policy

---
+++ src/back/ng-pokemon.API/Program.Cors.cs
@@ -0,0 +1,24 @@
+﻿namespace ng_pokemon.API;
+
+public static partial class Program
+{
+    /// <summary>
+    /// Configures and adds a CORS policy to the application services.
+    /// This policy allows requests from a specific origin and allows any HTTP method and header.
+    /// </summary>
+    /// <param name="builder">The WebApplicationBuilder used to configure the application services.</param>
+    public static void AddCorsPolicy(WebApplicationBuilder builder)
+    {
+        builder.Services.AddCors(options =>
+        {
+            string corsPolicy = "corsPolicy";
+            string allowedOrigin = builder.Configuration["CorsSettings:AllowedOrigin"] ?? "https://default-origin.com";
+            options.AddPolicy(name: corsPolicy, policyBuilder =>
+            {
+                policyBuilder.WithOrigins(allowedOrigin)
+                             .AllowAnyHeader()
+                             .AllowAnyMethod();
+            });
+        });
+    }
+}
--- src/back/ng-pokemon.API/Program.cs
+++ src/back/ng-pokemon.API/Program.cs
@@ -6,55 +6,71 @@ using ng_pokemon.Application;
 using ng_pokemon.Infrastructure;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-var storagePath = builder.Configuration["Storage:PhysicalPath"];
-var storageRequestPath = "/storage";
-string? connectionString = builder.Configuration.GetConnectionString("ng-pokemon");
+namespace ng_pokemon.API;
 
-if (string.IsNullOrEmpty(connectionString))
-    throw new InvalidOperationException("Missing ng-pokemon in appsettings.json");
-if (string.IsNullOrEmpty(storagePath))
-    throw new InvalidOperationException("Missing storage path in appsettings.json");
-
-builder.Host.UseSerilog((context, configuration) =>
-            configuration.ReadFrom.Configuration(context.Configuration));
-
-// Add services to the container.
-builder.Services.AddInfrastructure(connectionString);
-builder.Services.AddApplication();
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+/// <summary>
+/// Main class Program
+/// </summary>
+public static partial class Program
 {
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+    /// <summary>
+    /// Main function to run the program
+    /// </summary>
+    public static void Main(string[] args)
+    { 
+        var builder = WebApplication.CreateBuilder(args);
+        var storagePath = builder.Configuration["Storage:PhysicalPath"];
+        var storageRequestPath = "/storage";
+        string? connectionString = builder.Configuration.GetConnectionString("ng-pokemon");
 
-var app = builder.Build();
+        if (string.IsNullOrEmpty(connectionString))
+            throw new InvalidOperationException("Missing ng-pokemon in appsettings.json");
+        if (string.IsNullOrEmpty(storagePath))
+            throw new InvalidOperationException("Missing storage path in appsettings.json");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        AddCorsPolicy(builder);
+
+        builder.Host.UseSerilog((context, configuration) =>
+                    configuration.ReadFrom.Configuration(context.Configuration));
+
+        // Add services to the container.
+        builder.Services.AddInfrastructure(connectionString);
+        builder.Services.AddApplication();
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(storagePath),
+            RequestPath = new PathString(storageRequestPath)
+        });
+
+        app.UseHttpsRedirection();
+
+        app.UseSerilogRequestLogging();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+        app.Run();
+    }
 }
-
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = new PhysicalFileProvider(storagePath),
-    RequestPath = new PathString(storageRequestPath)
-});
-
-app.UseHttpsRedirection();
-
-app.UseSerilogRequestLogging();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.UseMiddleware<ExceptionHandlingMiddleware>();
-
-app.Run();
